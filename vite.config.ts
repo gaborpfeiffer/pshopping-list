7745cc9ce8fd6e1ecb239dae26c235e3ab6779fe
Gabor Pfeiffer <gaborpfeiffer221@gmail.com>
2024-06-01 14:10:25 +0200

    feature: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,51 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touc-icon.png'], //'masked-icon.svg'
+  manifest: {
+    name: 'P+ShoppingList',
+    short_name: 'PShopList',
+    description: 'Single page shopping list app',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      // {
+      //   src: '/maskable_icon.png',
+      //   sizes: '512x512',
+      //   type: 'image/png',
+      //   purpose: 'any maskable',
+      // },
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/pshopping-list',
+    start_url: '/pshopping-list',
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pshopping-list/',
-  plugins: [
-    react(),
-    // VitePWA({
-    //   manifest: {
-    //     name: 'My Awesome App',
-    //     short_name: 'MyApp',
-    //     description: 'My Awesome App description',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: 'pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    // }),
-  ],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 });
